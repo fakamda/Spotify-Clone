c6d351ba28ad43ac14f0843f5bf2dec674680ede
fakamda <facundoelcardenal@gmail.com>
2023-12-03 23:18:04 -0300

    a

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -22,12 +22,15 @@ export function Player () {
     const [currentSong, setCurrentSong] = useState(null)
     const audioRef = useRef()
 
+    useEffect(() => {
+        audioRef.current.src = `/music/2/01.mp3`
+    }, [])
+
     const handleClick = () => {
         if (isPlaying) {
             audioRef.current.pause()
             console.log("audio pause")
         } else {
-            audioRef.current.src = `/music/1/01.mp3`
             audioRef.current.play()
             audioRef.current.volume = 0.1
             console.log("audio play")
