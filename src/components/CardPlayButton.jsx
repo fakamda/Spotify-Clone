90ac581ab85251c63739884f414e824e49a67413
fakamda <facundoelcardenal@gmail.com>
2023-12-03 23:33:02 -0300

    player button sync

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -3,9 +3,14 @@ import { usePlayerStore } from "../store/playerStore"
 
 export function CardPlayButton ({ id }) {
     const { currentSong, isPlaying, setIsPlaying, setCurrentSong } = usePlayerStore(state => state)
+    
+    const handleClick = () => {
+        setIsPlaying(!isPlaying)
+    }
+
     return (
-        <div className="card-play-button rounded-full bg-green-500 p-4">
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
             {isPlaying ? <Pause /> : <Play />}
-        </div>
+        </button>
     )
 }
