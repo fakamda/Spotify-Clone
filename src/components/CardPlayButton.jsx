0d8b4186a1dc773a47cc36f00d0d61262fb696c6
fakamda <facundoelcardenal@gmail.com>
2023-12-04 02:54:10 -0300

    api songs config playlist ok player current song

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,16 +1,30 @@
 import { Pause, Play } from "./Player"
-import { usePlayerStore } from "../store/playerStore"
+import { usePlayerStore } from "../store/playerStore" //ESTADO GLOBAL DE ZUSTAND
 
 export function CardPlayButton ({ id }) {
-    const { currentSong, isPlaying, setIsPlaying, setCurrentSong } = usePlayerStore(state => state)
+    const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
+
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id //si esta reproduciendo y esta reproduciendo el id de la musica se muestra
     
     const handleClick = () => {
-        setIsPlaying(!isPlaying)
+        if(isPlayingPlaylist) {
+            setIsPlaying(false)
+            return
+        }
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const { songs, playlist } = data
+            setIsPlaying(true)
+            setCurrentMusic({ songs, playlist, song: songs[0] })
+        })
     }
+
+
 
     return (
         <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-            {isPlaying ? <Pause /> : <Play />}
+            {isPlayingPlaylist ? <Pause /> : <Play />}
         </button>
     )
 }
