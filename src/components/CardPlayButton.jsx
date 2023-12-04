536d453002608bc77773df5213689a652cfaeb79
fakamda <facundoelcardenal@gmail.com>
2023-12-03 23:27:30 -0300

    add zustand

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,9 +1,11 @@
 import { Pause, Play } from "./Player"
+import { usePlayerStore } from "../store/playerStore"
 
 export function CardPlayButton ({ id }) {
+    const { currentSong, isPlaying, setIsPlaying, setCurrentSong } = usePlayerStore(state => state)
     return (
         <div className="card-play-button rounded-full bg-green-500 p-4">
-            <Play />
+            {isPlaying ? <Pause /> : <Play />}
         </div>
     )
 }
